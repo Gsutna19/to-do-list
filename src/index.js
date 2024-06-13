a353859035abce272239ffdfc24d27d81eaf2e56
Gsutna19 <fe.lipe19menezes@gmail.com>
2024-06-13 10:34:32 -0400

    Finilize set up for webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import _ from 'lodash';
+import './style/style.css';
+import Icon from './images/icon.jpg';
+import printMe from './print';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     //Lodash, now imported for this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console.';
+    btn.onclick = printMe;
+
+    // Add image to our existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(btn);
+    element.appendChild(myIcon);
 
     return element;
 }
